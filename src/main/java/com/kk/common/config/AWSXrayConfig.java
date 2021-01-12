4b70b50aa600e953a359307f8b6df8eaa72b74ff
AE200403 <AE200403>
2021-01-12 17:28:22 +0900

    x-ray 설정 추가

---
+++ src/main/java/com/kk/common/config/AWSXrayConfig.java
@@ -0,0 +1,28 @@
+package com.kk.common.config;
+
+import com.amazonaws.xray.AWSXRay;
+import com.amazonaws.xray.AWSXRayRecorder;
+import com.amazonaws.xray.AWSXRayRecorderBuilder;
+import com.amazonaws.xray.plugins.EC2Plugin;
+import com.amazonaws.xray.plugins.ElasticBeanstalkPlugin;
+import com.amazonaws.xray.strategy.DefaultStreamingStrategy;
+import com.amazonaws.xray.strategy.sampling.LocalizedSamplingStrategy;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.net.URL;
+
+@Configuration
+public class AWSXrayConfig {
+
+    @PostConstruct
+    public void init() {
+        AWSXRayRecorderBuilder builder = AWSXRayRecorderBuilder.standard().withPlugin(new EC2Plugin());
+        URL ruleFile = AWSXrayConfig.class.getResource("aws-xray/rules.json");
+        builder.withSamplingStrategy(new LocalizedSamplingStrategy(ruleFile));
+        AWSXRayRecorder globalRecorder = builder.build();
+        AWSXRay.setGlobalRecorder(globalRecorder);
+    }
+}
